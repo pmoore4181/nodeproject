9c29991e5628bb7fe454e1a8b0a0361dc1d274b9
Patrick Moore <pmoore4181@gmail.com>
2017-06-19 04:21:54 +0000

    added routing with express

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,1 +1,15 @@
-console.log("hey bobes");
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("Hello World!");
+});
+
+app.get('/routing', function(req, res){
+    res.send("Howdy Routing World!");
+});
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log('the server is running on port: ' + port);
+});
